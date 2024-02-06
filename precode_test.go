96640b1596412752434fba1b3933dd5c89d13181
Nosov Alexandr <nosov3006@inbox.ru>
2024-02-06 12:01:50 +0300

    fix remarks

--- precode_test.go
+++ precode_test.go
@@ -3,51 +3,12 @@ package tests
 import (
 	"net/http"
 	"net/http/httptest"
-	"strconv"
 	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
-
-var cafeList = map[string][]string{
-	"moscow": []string{"Мир кофе", "Сладкоежка", "Кофе и завтраки", "Сытый студент"},
-}
-
-func mainHandle(w http.ResponseWriter, req *http.Request) {
-	countStr := req.URL.Query().Get("count")
-	if countStr == "" {
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte("count missing"))
-		return
-	}
-
-	count, err := strconv.Atoi(countStr)
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte("wrong count value"))
-		return
-	}
-
-	city := req.URL.Query().Get("city")
-
-	cafe, ok := cafeList[city]
-	if !ok {
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte("wrong city value"))
-		return
-	}
-
-	if count > len(cafe) {
-		count = len(cafe)
-	}
-
-	answer := strings.Join(cafe[:count], ",")
-
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(answer))
-}
 
 // Тест для проверки ответа, когда в параметре city больше кафе, чем в данном городе
 func TestMainHandlerWhenCountMoreThanTotal(t *testing.T) {
@@ -80,8 +41,8 @@ func TestMainHandlerWhenWrongCityValue(t *testing.T) {
 	res := responseRecorder.Body.String()
 
 	// проверки через пакет assert, чтобы проверить два условия сразу
-	assert.Equal(t, responseRecorder.Code, http.StatusBadRequest)
-	assert.Equal(t, res, rightResponse)
+	assert.Equal(t, http.StatusBadRequest, responseRecorder.Code)
+	assert.Equal(t, rightResponse, res)
 }
 
 // проверка статуса и тела ответа сервера, при корректном запросе
@@ -95,6 +56,6 @@ func TestMainHandlerWhenOk(t *testing.T) {
 	res := responseRecorder.Body.String()
 
 	// проверки через пакет assert, чтобы проверить два условия сразу
-	assert.Equal(t, responseRecorder.Code, http.StatusOK)
-	assert.NotNil(t, res)
+	assert.Equal(t, http.StatusOK, responseRecorder.Code)
+	assert.NotEmpty(t, res)
 }
